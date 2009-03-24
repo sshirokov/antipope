cd8134c56abdfc18e12c173c7ae78f7cd29b6f44
Yaroslav Shirokov <yshirokov@yaroslav-shirokovs-macbook-pro.local>
2009-03-24 02:27:09 -0400

    Added scaffolding to adding manage tasks

--- src/manage.py
+++ src/manage.py
@@ -1,1 +1,21 @@
+import sys
 print "manage.py running"
+
+class UserError(Exception): pass
+
+if __name__ == '__main__':
+    print "Manage running"
+    (command, args) = sys.argv[0], sys.argv[1:]
+    print "Command: '%s'" % command
+
+    if command == 'help':
+        if not(len(args) >= 1): raise UserError("Need arguments for help")
+        try: __import__('tasks.%s' % args[0], globals(), locals(), ['tasks']).help(args[1:])
+        except ImportError: raise UserError("No such command: %s" % args[0])
+    else:
+        try: __import__('tasks.%s' % command, globals(), locals(), ['tasks']).run(args)
+        except ImportError: raise UserError("No such command: %s" % command)
+        
+
+        
+    print "Manage completed"
---
+++ src/tasks/test.py
@@ -0,0 +1,7 @@
+def run(args):
+    print "Testing command executed"
+    print "\targs(%s)" % args
+
+def help(*args):
+    print "Test help executed"
+    print "\targs(%s)" % args
