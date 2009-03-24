4eeaf423b87ee71dcf493d8f833e9d70070b3ebd
Yaroslav Shirokov <yshirokov@yaroslav-shirokovs-macbook-pro.local>
2009-03-24 02:48:48 -0400

    manage command dispatch works

--- src/manage.py
+++ src/manage.py
@@ -1,21 +1,21 @@
+#!/usr/bin/env python
 import sys
-print "manage.py running"
 
 class UserError(Exception): pass
 
-if __name__ == '__main__':
-    print "Manage running"
-    (command, args) = sys.argv[0], sys.argv[1:]
-    print "Command: '%s'" % command
+def usage():
+    print "Manage must be called with a command as an argument."
+    sys.exit(0)
 
-    if command == 'help':
-        if not(len(args) >= 1): raise UserError("Need arguments for help")
+if __name__ == '__main__':
+    self, args = sys.argv[0], sys.argv[1:]
+    if len(args) < 1: usage()
+    (command, args) = args[0], args[1:]
+
+    if command == 'help' and ((len(args) > 0) or ussage()):
         try: __import__('tasks.%s' % args[0], globals(), locals(), ['tasks']).help(args[1:])
         except ImportError: raise UserError("No such command: %s" % args[0])
     else:
         try: __import__('tasks.%s' % command, globals(), locals(), ['tasks']).run(args)
         except ImportError: raise UserError("No such command: %s" % command)
         
-
-        
-    print "Manage completed"
