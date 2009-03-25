e1655023fca709e2e0a1f29dad66e01e8e4a7d9f
Yaroslav Shirokov <yshirokov@yaroslav-shirokovs-macbook-pro.local>
2009-03-25 18:04:04 -0400

    Not poluting the import space

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,5 +1,5 @@
 import os, re, inspect
 
-pwd = os.path.dirname(inspect.currentframe().f_code.co_filename)
+_pwd = os.path.dirname(inspect.currentframe().f_code.co_filename)
 tasklist = [f.split('.')[0] for f in
-         filter(lambda f: re.search('^[^_]\w+\\.py$', f), os.listdir(pwd))]
+         filter(lambda f: re.search('^[^_]\w+\\.py$', f), os.listdir(_pwd))]
