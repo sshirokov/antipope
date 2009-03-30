fbeb1b42d503d456caf85a263c64743023fb2b73
Yaroslav Shirokov <yshirokov@yaroslav-shirokovs-macbook-pro.local>
2009-03-30 18:02:44 -0400

    URL generation function

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
 POST_ROOT = 'posts'
 META_FILE = 'meta.yaml'
+POST_FILE = 'main.org'
 OUTPUT_BASE = POST_ROOT + ".out"
--- src/util/article.py
+++ src/util/article.py
@@ -6,6 +6,7 @@ from util.slicer import Slicer
 from util import ObjDict
 
 class ArticleError(Exception): pass
+class ArticleSystemError(ArticleError): pass
 
 class Article(object):
     '''
@@ -31,11 +32,23 @@ class Article(object):
         return parser().parse(self.meta.date)
 
     @property
-    def path(self):
-        return os.path.join(str(self.date.year),
-                            self.date.strftime("%m"),
-                            self.date.strftime("%d"),
-                            self.slug)
+    def path(self): return self.get_path()
+    
+    def get_path(self, type = 'url', style = 'relative'):
+        stderror = lambda type, style, message = '': ArticleSystemError("Cannot return a path of %s/%s%s" % (type, style, message and ": %s" % message))
+        if not (type in ('url', 'os') and style in ('relative', 'absolute')): raise stderror(type, style)
+        if type == 'url':
+            if style == 'absolute':
+                try: return "%s/%s" % (settings.BASE_URL, self.get_path('url', 'relative'))
+                except AttributeError: raise stderror(type, style, "No settings.BASE_URL")
+            elif style == 'relative':
+                return "/".join([str(self.date.year),
+                                    self.date.strftime("%m"),
+                                    self.date.strftime("%d"),
+                                    self.slug])
+        elif type == 'os':
+            if style == 'absolute': return os.path.join(os.getcwd(), self.get_path('os', 'relative'))
+            elif style == 'relative': return os.path.join(settings.POST_ROOT, self.slug)
     
     def build(self, dest):
         return True
