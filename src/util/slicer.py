c3f3e3ead3a7d9c0ea19ad8cff563b7272d5a393
Yaroslav Shirokov <yshirokov@yaroslav-shirokovs-macbook-pro.local>
2009-04-01 21:24:09 -0400

    Slicer now has .objects() to return objeect lists

--- src/util/slicer.py
+++ src/util/slicer.py
@@ -10,8 +10,18 @@ class Slicer(object):
                           (getattr(a1, key) < getattr(a2, key) and -1) or \
                           (getattr(a1, key) == getattr(a2, key) and 0) or \
                           (getattr(a1, key) > getattr(a2, key) and 1))
+
+    @classmethod
+    def slugs_to_articles(self, slug_list, prune = False):
+        def convert(slug):
+            from util.article import Article, ArticleError
+            try: return Article(slug)
+            except ArticleError: return None
+        articles = map(convert, slug_list)
+        if prune: articles = filter(None, articles)
+        return articles
     
-    class slices:
+    class slices(object):
         '''
         Contains the user-accessed slice returning methods
         All slice returning methods return lists of tags"
@@ -26,3 +36,9 @@ class Slicer(object):
             return filter(lambda a: \
                               Parser.is_article(a) and Parser.is_published(a),
                           os.listdir(settings.POST_ROOT))
+
+    class objects(object): pass
+
+#Ugly, and should probably be done better, somehow
+for method in filter(lambda s: '__' not in s, dir(Slicer.slices)):
+    setattr(Slicer.objects, method, staticmethod(lambda *a, **k: Slicer.slugs_to_articles(getattr(Slicer.slices, method)(*a, **k))))
