ed1f31f0b6685d19fcc651eec71966b3fd60f44f
Yaroslav Shirokov <yshirokov@yaroslav-shirokovs-macbook-pro.local>
2009-04-04 12:35:33 -0400

    Extended task search to [\w_-]+

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -2,4 +2,4 @@ import os, re, inspect
 
 _pwd = os.path.dirname(inspect.currentframe().f_code.co_filename)
 tasklist = [f.split('.')[0] for f in
-         filter(lambda f: re.search('^[^_]\w+\\.py$', f), os.listdir(_pwd))]
+         filter(lambda f: re.search('^[^_][\w\-_]+\\.py$', f), os.listdir(_pwd))]
